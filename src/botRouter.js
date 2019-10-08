e1aa1d2693972e878ac7102a99f84d1d2a2516ce
Brendan Meehan <bmmeehan3@gmail.com>
2019-10-08 00:46:51 -0400

    oof testingq

--- src/botRouter.js
+++ src/botRouter.js
@@ -2,13 +2,16 @@ const express = require('express')
 const bot = require('./botScript')
 
 const botRouter = express.Router()
-
+const jsonParser = express.json()
 
 botRouter.route('/')
 .get((req, res) => {
-    res.json('test')
+    let test = {
+        text: "here"
+    }
+    res.json(test)
 })
-.post((req, res) => {
+.post(jsonParser, (req, res, next) => {
     bot.response(req);
     
 });
