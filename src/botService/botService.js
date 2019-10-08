8e17f9f561786ad80a17211dd5bae03cff2632e0
Brendan Meehan <bmmeehan3@gmail.com>
2019-10-08 03:19:50 -0400

    fixed db connection error

--- src/botService/botService.js
+++ src/botService/botService.js
@@ -1,6 +1,6 @@
 const botService = {
     getResponse(db) {
-        return db.select('*').from('bot_response').where('id', 1)
+        return db.select('*').from('bot_response')
     },
     addNewResponse(db, res) {
         return db.insert(res).into('bot_response').returning('*').then(rows => {
